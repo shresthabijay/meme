f6f157e94af11c731cbf0c27a5772114f5fc8886
shresthabijay <sbijay777@gmail.com>
2019-02-02 20:58:25 +0545

    sad

--- meme.js
+++ meme.js
@@ -1,3 +1,3 @@
-let meme=["nani","re","g-bitch","manigga","manigga2","mom","r-nigga","sad-violin","tadah","surprise","terror","baka"]
+let meme=["nani","re","g-bitch","manigga","manigga2","mom","r-nigga","sad-violin","tadah","surprise","terror","baka","johncena","no","spaghet"]
 
 module.exports=meme
