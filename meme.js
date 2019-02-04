15ee4613f589b854883e57b75917b2eba4130bcb
shresthabijay <sbijay777@gmail.com>
2019-02-04 12:04:48 +0545

    dasd

--- meme.js
+++ meme.js
@@ -1,3 +1,3 @@
-let meme=["nani","re","g-bitch","manigga","manigga2","mom","r-nigga","sad-violin","tadah","surprise","terror","baka","johncena","no","spaghet"]
+let meme=["nani","re","g-bitch","manigga","manigga2","mom","r-nigga","sad-violin","tadah","surprise","terror","baka","johncena","no","spaghet","fuckthis","shutup"]
 
 module.exports=meme
